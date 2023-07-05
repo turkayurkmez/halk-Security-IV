633d84aaa4601620fa0b19828803c31651344327
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-07-05 15:29:06 +0300

    Anti forgery token in mvc & validation & white / black list explained

---
+++ CSRFWithMVC/CSRFWithMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSRFWithMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CSRFWithMVC/CSRFWithMVC/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CSRFWithMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public int Rating { get; set; }
+    }
+}
